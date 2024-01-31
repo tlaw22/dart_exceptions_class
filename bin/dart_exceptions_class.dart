5da91130d9ff92fd0f92dd7240bd6c902cd4a4f1
tim <timothy.lawless@gmail.com>
2024-01-31 12:43:33 -0500

    first commit

---
+++ bin/dart_exceptions_class.dart
@@ -0,0 +1,27 @@
+void main(List<String> arguments) {
+  try {
+    Cat cat1 = Cat(-5);
+    print(cat1.age);
+  }on ageExecpt catch (e) {
+    print(e.message);
+  }
+}
+
+class ageExecpt implements Exception {
+  String message = "Age Exception";
+  ageExecpt({this.message='Age Exception'});
+  @override
+  String toString() => "Age Exception: ${message}";
+}
+
+class Cat {
+  int age = 0;
+  Cat(int age) {
+    if (age < 0) {
+      throw ageExecpt(message: 'Age cannot be negative');
+      // print("Age cannot be negative");
+    } else {
+      this.age = age;
+    }
+  }
+}
---
+++ test/dart_exceptions_class_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exceptions_class/dart_exceptions_class.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
